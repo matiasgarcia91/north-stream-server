77fe203d3fe61c7cf732618c7ab8de9f765d1bb2
Matias Garcia <matiasigarcia91@gmail.com>
2020-05-31 13:48:11 +0200

    clean up

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const User = require("./models").user;
 const userRouter = require("./user-router");
 const cors = require("cors");
 
 const port = process.env.PORT || 4001;
-// const index = require("./routes/index");
 
 const app = express();
 app.use(cors());
-// app.use(index);
 app.use(express.json());
 
 const server = http.createServer(app);
@@ -23,24 +20,16 @@ app.use((req, res, next) => {
 });
 
 app.use(userRouter);
+
 let interval;
 
 io.on("connection", socket => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("connect", () => {
+    console.log("Client Connected");
+  });
   socket.on("disconnect", () => {
     console.log("Client disconnected");
-    clearInterval(interval);
   });
 });
-
-const getApiAndEmit = socket => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
-};
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
