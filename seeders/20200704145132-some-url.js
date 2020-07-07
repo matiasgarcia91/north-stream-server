22ed3356612e93ec41aba00fa6ef23d6bd8f14da
Matias Garcia <matiasigarcia91@gmail.com>
2020-07-07 21:32:48 +0200

    add test users

--- seeders/20200704145132-some-url.js
+++ seeders/20200704145132-some-url.js
@@ -4,7 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("streamUrls", [
       {
-        url: "435313823",
+        url: "435883644",
       },
     ]);
   },
