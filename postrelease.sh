01f3057421b61cb276e299a0cbdae509928821db
Matias Garcia <matiasigarcia91@gmail.com>
2020-07-13 22:38:52 +0200

    add retailers users

--- postrelease.sh
+++ postrelease.sh
@@ -4,3 +4,4 @@ npx sequelize-cli db:seed --seed 20200704145132-some-url
 npx sequelize-cli db:seed --seed 20200713195705-nuestros
 npx sequelize-cli db:seed --seed 20200713182215-general-users
 npx sequelize-cli db:seed --seed 20200713190330-only-mystic
+npx sequelize-cli db:seed --seed 20200713191709-retailers
