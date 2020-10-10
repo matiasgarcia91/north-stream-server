c1ff4aa84fe18db445e47be5ada63972c5ae9e07
Matias Garcia <matiasigarcia91@gmail.com>
2020-10-10 16:11:22 +0200

    update post-release script

--- postrelease.sh
+++ postrelease.sh
@@ -1,8 +1,4 @@
 npx sequelize-cli db:migrate:undo:all
 npx sequelize-cli db:migrate
 npx sequelize-cli db:seed --seed 20200704145132-some-url
-npx sequelize-cli db:seed --seed 20200713195705-nuestros
-npx sequelize-cli db:seed --seed 20200713182215-general-users
-npx sequelize-cli db:seed --seed 20200713190940-only-north
-npx sequelize-cli db:seed --seed 20200713191709-retailers
-npx sequelize-cli db:seed --seed 20200716071108-second-group
+npx sequelize-cli db:seed --seed 20201010135855-users-14
